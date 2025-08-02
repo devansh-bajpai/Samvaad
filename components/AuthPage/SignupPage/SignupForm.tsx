fb6f11ab2a19471aa50e306f848003bff4000a66
Devansh Bajpai <devanshb.code@gmail.com>
2025-08-02 12:37:25 +0530

    Added signup logic

--- components/AuthPage/SignupPage/SignupForm.tsx
+++ components/AuthPage/SignupPage/SignupForm.tsx
@@ -1,106 +1,138 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm, SubmitHandler } from "react-hook-form";
 import { motion, AnimatePresence } from "framer-motion";
+import { auth } from '@/lib/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
 
 type Inputs = {
-  email: string,
-  password: string,
-  confirmPassword: string,
+    email: string,
+    password: string,
+    confirmPassword: string,
 };
 
 export default function SignupForm() {
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
 
-  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+    const { register, handleSubmit, setError, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        setLoading(true);
+        if (data.password != data.confirmPassword) {
+            setLoading(false);
+            setError("confirmPassword", {
+                type: "manual",
+                message: "Passwords do not match."
+            })
+            return;
+        }
+        try {
+            await createUserWithEmailAndPassword(auth, data.email, data.password);
+            router.replace("/home")
+        }
+        catch {
+            setLoading(false);
+            setError("confirmPassword", {
+                type: "manual",
+                message: "There was an error signing up"
+            })
+            return;
 
-  console.log(watch("email"))
+        }
+        finally {
+            setLoading(false);
+        }
+    }
 
-  return (
-    <>
-  
-      <form className='flex flex-col justify-center items-center w-full' onSubmit={handleSubmit(onSubmit)}>
+    return (
+        <>
 
-        <h2 className='text-3xl mb-5'>Sign Up</h2>
-        
-        <input type='email' placeholder='Email'
-          className='input m-2 focus:outline-none'
-          {...register("email", {
-            required: "An email is required",
-            pattern: { value: /^\S+@\S+$/, message: "Invalid email format" }
-          })}
-          />
+            <form className='flex flex-col justify-center items-center w-full' onSubmit={handleSubmit(onSubmit)}>
 
-        {/* {errors.email && <span className='text-red-500 opacity-0 animate-fadeIn'>{errors.email?.message}</span>} */}
+                <h2 className='text-3xl mb-5'>Sign Up</h2>
 
-          <AnimatePresence mode="wait">
-  {errors.email && (
-    <motion.p
-      key="email-error"
-      initial={{ opacity: 0, y: -5 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: -5 }}
-      transition={{ duration: 0.3 }}
-      className="text-red-500 text-sm select-none"
-    >
-      {errors.email.message}
-    </motion.p>
-  )}
-</AnimatePresence>
+                <input type='email' placeholder='Email'
+                    className='input m-2 focus:outline-none'
+                    {...register("email", {
+                        required: "An email is required",
+                        pattern: { value: /^\S+@\S+$/, message: "Invalid email format" }
+                    })}
+                />
 
+                {/* {errors.email && <span className='text-red-500 opacity-0 animate-fadeIn'>{errors.email?.message}</span>} */}
 
-
-        
-        <input type='password' placeholder='Password' className='input m-2 focus:outline-none' {...register("password", { required: "Password is required", minLength: 
-          {value: 8, message: "Password must be atleast 8 characters long."}
-         })} />
-        {/* {errors.password && <span className='text-red-500'>{errors.password?.message}</span>} */}
-
-         <AnimatePresence mode="wait">
-  {errors.password && (
-    <motion.p
-      key="password-error"
-      initial={{ opacity: 0, y: -5 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: -5 }}
-      transition={{ duration: 0.3 }}
-      className="text-red-500 text-sm select-none"
-    >
-      {errors.password?.message}
-    </motion.p>
-  )}
-</AnimatePresence>
-
-
-
-
-  <input type='password' placeholder='Confirm Password' className='input m-2 focus:outline-none' {...register("confirmPassword", { required: "Password is required", minLength: 
-          {value: 8, message: "Password must be atleast 8 characters long."}
-         })} />
-        {/* {errors.password && <span className='text-red-500'>{errors.password?.message}</span>} */}
-
-         <AnimatePresence mode="wait">
-  {errors.password && (
-    <motion.p
-      key="confirm-password-error"
-      initial={{ opacity: 0, y: -5 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: -5 }}
-      transition={{ duration: 0.3 }}
-      className="text-red-500 text-sm select-none"
-    >
-      {errors.confirmPassword?.message}
-    </motion.p>
-  )}
-</AnimatePresence>
+                <AnimatePresence mode="wait">
+                    {errors.email && (
+                        <motion.p
+                            key="email-error"
+                            initial={{ opacity: 0, y: -5 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: -5 }}
+                            transition={{ duration: 0.3 }}
+                            className="text-red-500 text-sm select-none"
+                        >
+                            {errors.email.message}
+                        </motion.p>
+                    )}
+                </AnimatePresence>
 
 
 
 
-        <input className='btn btn-primary mt-4 hover:scale-105 transition duration-500' type="submit" />
-      </form>
+                <input type='password' placeholder='Password' className='input m-2 focus:outline-none' {...register("password", {
+                    required: "Password is required", minLength:
+                        { value: 8, message: "Password must be atleast 8 characters long." }
+                })} />
+                {/* {errors.password && <span className='text-red-500'>{errors.password?.message}</span>} */}
+
+                <AnimatePresence mode="wait">
+                    {errors.password && (
+                        <motion.p
+                            key="password-error"
+                            initial={{ opacity: 0, y: -5 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: -5 }}
+                            transition={{ duration: 0.3 }}
+                            className="text-red-500 text-sm select-none"
+                        >
+                            {errors.password?.message}
+                        </motion.p>
+                    )}
+                </AnimatePresence>
 
 
-    </>
-  )
+
+
+                <input type='password' placeholder='Confirm Password' className='input m-2 focus:outline-none' {...register("confirmPassword", {
+                    required: "Password is required", minLength:
+                        { value: 8, message: "Password must be atleast 8 characters long." }
+                })} />
+                {/* {errors.password && <span className='text-red-500'>{errors.password?.message}</span>} */}
+
+                <AnimatePresence mode="wait">
+                    {errors.confirmPassword && (
+                        <motion.p
+                            key="confirm-password-error"
+                            initial={{ opacity: 0, y: -5 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: -5 }}
+                            transition={{ duration: 0.3 }}
+                            className="text-red-500 text-sm select-none"
+                        >
+                            {errors.confirmPassword?.message}
+                        </motion.p>
+                    )}
+                </AnimatePresence>
+
+
+
+
+                <input className={`btn btn-primary mt-4 hover:scale-105 transition duration-500 ${loading ? "hidden" : ""}`} type="submit" />
+                <span className={`loading loading-spinner loading-md mt-4 ${loading ? "" : "hidden"}`}></span>
+            </form>
+
+
+        </>
+    )
 }
